bb568b5f3b86c987bc7c70e12018e52af0eb7887
Mistryr222 <mistryr222@gmail.com>
2025-03-24 22:10:01 -0400

    clean design and add functions

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,17 +1,44 @@
 'use client'
 
 import { useSession } from "next-auth/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Button, ButtonGroup, TextField } from "@mui/material";
 import html2canvas from "html2canvas";
+
+const typeCli = (line, setCurrentLine, addTypedLine, speed = 40) => {
+  return new Promise((resolve) => {
+    let i = 0;
+    const typed = `$ ${line}`;
+    
+    // Type out the interval one character at a time
+    const intervalId = setInterval(() => {
+      i++;
+      setCurrentLine(typed.slice(0, i));
+      if (i >= typed.length) {
+        clearInterval(intervalId);
+        addTypedLine(typed);
+        setCurrentLine("");
+        resolve();
+      }
+
+    }, speed);
+  })
+};
 
 export default function Dashboard() {
     const { data: session, status } = useSession();
 
     const [topItems, setTopItems] = useState([]);
-    const [timeRange, setTimeRange] = useState('long_term')
+    const [timeRange, setTimeRange] = useState('long_term');
     const [viewArtists, setViewArtists] = useState(true); 
     const [limit, setLimit] = useState(5);
+
+    const [typedlines, setTypedLines] = useState([]);
+    const [currentLine, setCurrentLine] = useState("");
+    const [showResults, setShowResults] = useState (false);
+
+    const prevTypeRef = useRef(viewArtists ? 'artists' : 'songs');
+    const prevRangeRef = useRef(timeRange);
 
     useEffect(() => {
         const fetchTopItems = async () => {
@@ -19,7 +46,6 @@ export default function Dashboard() {
 
             const endpoint = viewArtists ? 'artists' : 'tracks';
             const url = `https://api.spotify.com/v1/me/top/${endpoint}?limit=${limit}&time_range=${timeRange}`;
-
 
             const res = await fetch(url, {
                 headers: {
@@ -43,7 +69,6 @@ export default function Dashboard() {
       setLimit(val);
     };
 
-    // TODO make a loading.js file
     if (status === 'loading') {
         return <p>Loading...</p>
     }
@@ -54,27 +79,34 @@ export default function Dashboard() {
 
     const username = session?.user?.name || "username";
 
+    const ranges = {
+      "short_term": "past_month",
+      "medium_term": "past_six_months",
+      "long_term": "past_year"
+    };
+    
+    const getRangeFormat = (range) => {
+      return ranges[range] ?? range;
+    }
+
     return (
         <div className="min-h-screen w-full bg-gray-800 text-gray-100 font-mono flex items-center justify-center p-4">
           
           {/* Main inner formatting */}
-          <div className="flex flex-col md:flex-row gap-8 max-w-4xl w-full mx-auto">
+          <div className="flex flex-col md:flex-row gap-12 max-w-5xl w-full mx-auto">
             <div className="bg-black w-full md:w-1/2 rounded-md overflow-hidden border border-gray-700 shadow-lg">
               <div className="bg-gray-800 px-3 py-2 flex justify-between items-center">
                 <span className="font-bold">cli-fy</span>
                 <div className="space-x-2">
-                  <button className="hover:bg-gray-700 px-2 py-1 rounded">-</button>
+                  <button className="hover:bg-gray-700 px-2 py-1 rounded">–</button>
                   <button className="hover:bg-gray-700 px-2 py-1 rounded">▢</button>
-                  <button className="hover:bg-red-600 px-2 py-1 rounded">x</button>
+                  <button className="hover:bg-red-600 px-2 py-1 rounded">X</button>
                 </div>
               </div>
 
-            {/* TODO change timerange to be dictionary mapped to names like 6 months */}
               <div className="p-4 text-white">
-                <pre className="mb-2">
-                {`cd /Users/${username}/${viewArtists ? 'top_artists' : 'top_songs'}/${timeRange}
-  ls`}
-                </pre>
+                <p>{`$ cd /Users/${username}/${viewArtists ? 'top_artists' : 'top_songs'}/${getRangeFormat(timeRange)}`}</p>
+                <p className="mb-2">$ ls</p>
 
                 <ul className="list-none space-y-2">
                   {topItems.map((item, index) => (
